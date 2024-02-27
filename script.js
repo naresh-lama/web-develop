c1728662ff626caf0bd613747c53d4685ec57421
naresh-lama <85601829+naresh-lama@users.noreply.github.com>
2024-02-28 00:03:26 +0900

    Add files via upload
    2nd days learning of data types

--- script.js
+++ script.js
@@ -1,1 +1,99 @@
-console.log("hello world")
+//
+
+// var name = "naresh";
+// let cast = "thapa";
+// const gender = "male";
+
+// console.log(name);
+// console.log(cast);
+// console.log(gender);
+
+
+// name = "dinesh";
+// console.log(name);
+
+// cast = "karki";
+// console.log(cast);
+
+// gender = "female";
+// console.log(gender);
+//const value can not change it will be sure that constant
+//yesma chahi change hunsakidaina if kunai chich lai change nai garna namile gari banaune ho bhane
+//const use garne
+
+
+//keywords
+//js le already use gareko words lai kyewords bhaninchha
+//already used words by is 
+
+///let var, const, for, if function,do while, console.log
+
+///these words are do not use for oure personal purpose
+
+
+/// data types
+/// 7 types of data type
+
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Null
+// 5.Undefined
+// 6.Array
+// 7.Objects
+
+
+let age1 = 20; //number data types
+let age2 = "naresh"; //Strings data types
+let age3 = true; // boolean data types is alwasy gives two types true or false
+let age4 = null; // null data types object
+let age5 = undefined; // kunai pani value store gareko chhaina
+let age6 = [1,2,3,4,5,6] // object
+let age7 = ["nepal","japana","India"]
+let age8 = ["nepal",true,true]
+let age9 = { //main object name is age9 //object
+    name: "naresh", // name is key naresh is value
+    age: 17,
+    area: "nepal",
+    classes: { //object bhitra ni arko object creat
+        grade: 9,
+        markse:[3,4,5],
+        subject:"mathmethics",
+    }
+}
+
+console.log(age1,age2,age4);
+
+console.log(age1, typeof age1);
+console.log(age2);
+console.log(age3);
+console.log(age4);
+console.log(age5);
+console.log(age6);
+console.log(age7);
+console.log(age8);
+console.log(age9);
+
+// data types.... null array object lai chahi java le object data bhanera dekhaidinchha
+
+// comments in js
+// there are 2 types of commnts in js
+// 1 single line commnet
+// 2 multiline comments
+// they are importants parst of any programing language it will be not compile and print out
+/// it will be use for while testing if there get make errorr and find out error
+// it will be also use for defination of or describe about code
+// single line will make bye double slash //
+/* multy commnets
+sotry
+comments
+describes
+make sure
+ */
+
+
+
+
+
+
+
