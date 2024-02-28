9b69891c8cdbbbe45c421f6ceb4ef9575889ea5d
LamaNaresh <lama@jfd.co.jp>
2024-02-29 00:28:25 +0900

    learn about operator

--- day_third/script.js
+++ day_third/script.js
@@ -1,30 +1,102 @@
-let age1 = 20; //number data types
-let age2 = "naresh"; //Strings data types
-let age3 = true; // boolean data types is alwasy gives two types true or false
-let age4 = null; // null data types object
-let age5 = undefined; // kunai pani value store gareko chhaina
-let age6 = [1,2,3,4,5,6] // object
-let age7 = ["nepal","japana","India"]
-let age8 = ["nepal",true,true]
-let age9 = { //main object name is age9 //object
-    name: "naresh", // name is key naresh is value
-    age: 17,
-    area: "nepal",
-    classes: { //object bhitra ni arko object creat
-        grade: 9,
-        markse:[3,4,5],
-        subject:"mathmethics",
-    }
-}
+/**
+2024-02-28 3rd day
+ */
 
-console.log(age1,age2,age4);
+// operators in js
+// statements and expression .... theoritical part bujnu matra parne
 
-console.log(age1, typeof age1);
-console.log(age2);
-console.log(age3);
-console.log(age4);
-console.log(age5);
-console.log(age6);
-console.log(age7);
-console.log(age8);
-console.log(age9);
+// 1.Arirthemetic Operator
+// 2. Assignment Operatotr
+// 3. Comparision Operator
+// 4. Logical Operator
+// 5. Ternary Operattor
+// 6. Bitwise Operator... sytanx matra herne
+
+
+
+/// statements and expression
+// kunai pari variable lai declair garna ya statement garna lai statement bhaninchha
+
+// let name = "naresh"; // code lai defined garnu lai statement
+//const sum = 5 * 6; // 5 * 6 experession bhaneko kunai pani statement lai operator garnu lai bhaninchha
+/// 7>3
+/// 8=3
+
+/// Operator in js
+// 1. Arithmetic operator .... +,*,-,/,%
+
+let sum = 4 + 5;
+let subs = 9 - 5;
+let multi = 8 * 5;
+let div = 84/4;
+let rmd = 100 % 3;
+
+// string lai ni join garne kam garchha 
+// string concatination
+
+let count1 = "Nepal";
+let count2 = "Australia";
+
+console.log("my permanent country is " + count1 + " now i am living in Australia" + count2);
+
+
+/// 2. Assignment operator
+// value assignment garna use garne operator
+// assignment operators ... +=,-=,*=,/=,%=
+
+let a = 2;
+console.log(a);
+
+a += 3; // a=a+3;
+console.log(a); // 5
+
+a -= 2; // a=a-2
+console.log(a); //3
+
+/// 3. Comparision Operatior  < = > <= >= == ===
+/// it alaways return value in boolean --- true false
+console.log(5 < 9);
+console.log(5 > 9);
+console.log(5 >=5);
+console.log(5 == 5);
+/// == equals nbut may not be of same data type
+///value matra check garchha data type check gardaina
+console.log(5 == "5"); true ///"5"string bhaye pani data type check nagarekole
+console.log(5 === 5);
+///=== equals and of the same data type value and data type are check if there exact value will assign
+console.log("5" === 5); /// false
+
+/// operator precedence and associativity 
+console.log(5 + 6 - (4 * 2) / 4) /// BADMAS
+
+
+/// Logical operator
+// AND OR NOT
+/// AND &&.... if both of the caseses are true true is returned in AND opeator
+// OR || ... if one of the cases is true true is retruned in OR operator
+// NOT ! ... if true return false  and vice versa
+
+console.log("&&");
+console.log(true && true);
+console.log(true && false);
+
+
+console.log("||");
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+console.log("!");
+console.log(!true);//false
+console.log(!false);///true
+
+// 5. Ternary operator
+// condition ? statement1 : statement2;
+
+let age = 18;
+console.log(age <= 20 ? "you can not drive" : "you can drive");
+let result = age <= 20 ? "you can not drive" : "you can drive";
+console.log(result);
+
+
+
