6ba5974ca844d3ee8aee8ce42ec58ed8740337f3
LamaNaresh <lama@jfd.co.jp>
2024-02-28 11:50:30 +0900

    day third web practice

---
+++ day_third/script.js
@@ -0,0 +1,30 @@
+let age1 = 20; //number data types
+let age2 = "naresh"; //Strings data types
+let age3 = true; // boolean data types is alwasy gives two types true or false
+let age4 = null; // null data types object
+let age5 = undefined; // kunai pani value store gareko chhaina
+let age6 = [1,2,3,4,5,6] // object
+let age7 = ["nepal","japana","India"]
+let age8 = ["nepal",true,true]
+let age9 = { //main object name is age9 //object
+    name: "naresh", // name is key naresh is value
+    age: 17,
+    area: "nepal",
+    classes: { //object bhitra ni arko object creat
+        grade: 9,
+        markse:[3,4,5],
+        subject:"mathmethics",
+    }
+}
+
+console.log(age1,age2,age4);
+
+console.log(age1, typeof age1);
+console.log(age2);
+console.log(age3);
+console.log(age4);
+console.log(age5);
+console.log(age6);
+console.log(age7);
+console.log(age8);
+console.log(age9);
