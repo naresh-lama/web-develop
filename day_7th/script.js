b4dea3aae00059be20e65bb471ca72265888add6
LamaNaresh <lama@jfd.co.jp>
2024-03-07 23:51:36 +0900

    leanr about loops in js

---
+++ day_7th/script.js
@@ -0,0 +1,75 @@
+//"use strict" /// error find out garna usefull hunchh. JavaScript kunai pani condition ma error lai ni print out gardinchha
+/// function declaration nagarikanai print out garchha
+country = "Nepal"; /// var let const use gareko chhaina
+console.log(country);
+
+/// mathiko conditon ma use stict use garema error throw garchha
+
+/// strict mode.... error haru kam garna usefull hunchha this a latest mode in JavaScirpt
+/// helps to tackle with errors by throwing error message
+
+
+
+/// looping
+// Entry control loop...for, while
+/// Exit control loop... do while
+
+/// 1. for looop .... kunai pani repeatation lai simple tarika le print out garna lai use garna sakinchha
+
+/// syntax  for(variable declaration; condition; increament/decreament) {
+    //logic
+//}
+
+for (let i = 0; i < 5; i++) {
+    console.log("I love my country");
+}
+
+/// i = 0, 0 < 5, 1....print 1
+/// i = 1, 1 < 5, 2....print 2
+/// i = 2, 2 < 5, 3....print 3
+/// i = 3, 3 < 5, 4....print 4
+/// i = 4, 4 < 5, 5....print 5
+/// i = 5, 5 < 5, false ....
+
+
+///while loop
+///syntax
+/**
+ while(condition) {
+    logic:
+    increamnet/decreamnet
+ }
+ */
+///variable chahi while ko mathi declare garne
+/// condtion () bhitra rakhne
+/// logic: ma chahi console.log
+/// increamnet/decreament : i++
+
+
+let y = 10;
+while(y < 20) {
+    console.log("I love my country");
+    y++;
+}
+
+// entry controle loop... surumai condtion check bhayera print out hune bhayekole entry control loop bhaninchha
+
+/// Do while loop ... exit control loop this loop will run atleast once a time
+/// condition true or false bhayepani ekpatak lai print out hunchha
+// syntax
+/**
+ do {
+    logic:
+ } while(condition);
+ */
+
+ let z = 30;
+ do {
+    console.log("I love my Country Nepal ... do while loop");
+    z++;
+ } while(z < 40);
+
+
+ /// arrays
+ /// arrays methods
+ /// objects
